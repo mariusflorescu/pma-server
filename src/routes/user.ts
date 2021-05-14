d396b2059ce4be2958cb727b79a8539c299e5f87
Marius Florescu <mariusflorescu23@icloud.com>
2021-05-15 01:01:24 +0300

    Added change of user description

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,9 @@
 import { Request, Response, Router } from "express";
 import Student from '../entities/Student';
 import Company from '../entities/Company'
+import isStudent from "../middleware/isStudent";
+import user from "../middleware/user";
+import isCompany from "../middleware/isCompany";
 
 const getAll = async (_:Request,res:Response) => {
   const students = await Student.find();
@@ -40,6 +43,44 @@ const getCompanyById = async (req:Request,res:Response) => {
   return res.json(company);
 }
 
+const updateStudentProfileDescription = async (req:Request,res:Response) => {
+  const {id,description} = req.body;
+
+  try{
+    const student = await Student.findOneOrFail({id});
+
+    if(res.locals.user.id !== id){
+      return res.status(401).json({error: "Not allowed"});
+    }
+
+    student.description = description;
+    await student.save();
+
+    return res.status(200).json(student);
+  }catch(e){
+    return res.status(500).json({error: 'Ahh... Something went wrong'});
+  }
+}
+
+const updateCompanyProfileDescription = async (req:Request,res:Response) => {
+  const {id,description} = req.body;
+
+  try{
+    const company = await Company.findOneOrFail({id});
+
+    if(res.locals.user.id !== id){
+      return res.status(401).json({error: "Not allowed"});
+    }
+
+    company.description = description;
+    await company.save();
+
+    return res.status(200).json(company);
+  }catch (e){
+    return res.status(500).json({error: 'Ahh... Something went wrong'});
+  }
+}
+
 const router = Router();
 
 router.get('/',getAll);
@@ -47,5 +88,7 @@ router.get('/students',getStudents);
 router.get('/companies',getCompanies);
 router.get('/student/:id',getStudentById);
 router.get('/company/:id',getCompanyById);
+router.put('/student/description',user,isStudent,updateStudentProfileDescription);
+router.put('/company/description',user,isCompany,updateCompanyProfileDescription);
 
 export default router;
